8b5d449741212e93ee7506c7fd619ac8fdeed965
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-12 22:27:50 +0530

    Majority Element n/3 optimal approach

--- Arrays-XIII.cpp
+++ Arrays-XIII.cpp
@@ -197,6 +197,51 @@ int main()
     cout << "\n";
     return 0;
 }
+//Time Complexity: O(N*logN)
+
+// Optimal approach
+vector<int> majorityElement(vector<int> v){
+    int n = v.size();
+
+    int cnt1 = 0, cnt2 = 0;
+    int el1 = INT_MIN;
+    int el2 = INT_MIN;
+
+    // applying the extended boyer moores voting algorithm
+    for(int i = 0; i < n; i++){
+        if(cnt1 == 0 && el2 != v[i]){
+            cnt = 1;
+            el1 = v[i];
+        }
+        else if(cnt2 == 0 && el1 != v[i]){
+            cnt2 = 1;
+            el2 = v[i];
+        }
+        else if(v[i] == el1) cnt1++;
+        else if(v[i] == el2) cnt2++;
+        else {
+            cnt1--;
+            cnt2--;
+        }
+    }
+    vector<int> ls;
+
+    // manually check if the stored elements in el1 and el2 are the majority elements
+    cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i < n; i++){
+        if(v[i] == el1) cnt1++;
+        if(v[i] == el2) cnt2++;
+    }
+    
+    int mini = int(n / 3) + 1;
+    if(cnt1 >= mini) ls.push_back(el1);
+    if(cnt2 >= mini) ls.push_back(el2);
+
+    return ls;
+}
+
+//Time Complexity: O(N) + O(N)
+
 int main(){
   return 0;
 }
